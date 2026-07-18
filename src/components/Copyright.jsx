695552921eb72925f9844cab7dc2f438b108841b
Paul Pogue <pogue.paulw@gmail.com>
2026-07-18 14:57:56 -0500

    redesign Service cards and footer

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -2,7 +2,7 @@ const Copyright = () => {
   const year = new Date().getFullYear();
 
   return (
-    <div className="text-center text-sm mt-6 opacity-40">
+    <div className="text-center text-sm pb-3 opacity-40">
       <p> &copy; {year} Pogue & Company CPAs PLLC. All Rights Reserved</p>
     </div>
   );
